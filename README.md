370ba7f3f66e5232c7b3f52a5651f1ac23e21a1b
wubbalubbadu <61597376+wubbalubbadu@users.noreply.github.com>
2023-02-08 22:03:51 -0600

    code

---
+++ README.md
@@ -0,0 +1,6 @@
+We will analyze the four bin packing algorithms for these following scenarios:
+
+1. All items are big
+2. All items are small
+3. Half items are big and half are small
+4. 
--- algs.py
+++ algs.py
@@ -1,60 +1,92 @@
+import random
 BIN_SIZE = 1
 
 
-def first_fit(items):
+def first_fit(i):
     bins = []
-    for item in items:
-        for bin in bins:
+    items = i.copy()
+    for i, item in enumerate(items):
+        for j, bin in enumerate(bins):
             if item + bin <= BIN_SIZE:
-                bin += item
+                bins[j] += item
                 break
         else:
             bins.append(item)
+    # print("first fit bins")
+    # print(bins)
+    # print(sum(bins))
     return len(bins)
 
-def first_fit_desc(items):
+def first_fit_desc(i):
     bins = []
+    items = i.copy()
     items.sort(reverse=True)
-    for item in items:
-        for bin in bins:
+    for i, item in enumerate(items):
+        for j, bin in enumerate(bins):
             if item + bin <= BIN_SIZE:
-                bin += item
+                bins[j] += item
                 break
         else:
             bins.append(item)
+   
+    # print("first fit desc bins")
+    # print(sum(bins))
+    # print(bins)
     return len(bins)
         
-def best_fit(items):
+def best_fit(i):
+    items = i.copy()
     bins = []
-    for item in items:
+    for i, item in enumerate(items):
+        best_bin_index = None
         best_bin = None
-        for bin in bins:
-            if item + bin <= BIN_SIZE and (best_bin is None or bin < best_bin):
+        for j, bin in enumerate(bins):
+            if item + bin <= BIN_SIZE and (best_bin is None or bin > best_bin):
+                best_bin_index = j
                 best_bin = bin
         if best_bin:
-            best_bin += item
+            bins[best_bin_index] += item
         else:
             bins.append(item)
+    # print("best fit bins")
+    # print(sum(bins))
+    # print(bins)
     return len(bins)
 
-def best_fit_desc(items):
-    bins = []
+def best_fit_desc(i):
+    items = i.copy()
     items.sort(reverse=True)
-    for item in items:
+    bins = []
+    for i, item in enumerate(items):
+        best_bin_index = None
         best_bin = None
-        for bin in bins:
-            if item + bin <= BIN_SIZE and (best_bin is None or bin < best_bin):
+        for j, bin in enumerate(bins):
+            if item + bin <= BIN_SIZE and (best_bin is None or bin > best_bin):
+                best_bin_index = j
                 best_bin = bin
         if best_bin:
-            best_bin += item
+            bins[best_bin_index] += item
         else:
             bins.append(item)
+    # print("best fit bins")
+    # print(sum(bins))
+    # print(bins)
+    return len(bins)
+    # print("best fit desc bins")
+    # print(sum(bins))
+    # print(bins)
     return len(bins)
 
 if __name__ == '__main__':
     # items = open('data.csv').readlines()
-    items = [0.3, 0.2, 0.5, 0.3, 0.8, 0.4, 0.2, 0.9, 0.1, 0.01, 0.135]
+    num_items = 30000
+    # items = [random.random() * 0.5 for i in range(num_items//2)] + [0.5 + random.random() * 0.5 for i in range(num_items//2)]
+    # print(sum(items))
+    items = [0.2, 0.5, 0.4, 0.7, 0.1, 0.3, 0.8]
     print("First Fit: number of bins used: %d" % first_fit(items))
+
     print("First Fit Descending: number of bins used: %d" % first_fit_desc(items))
+
     print("Best Fit: number of bins used: %d" % best_fit(items))
+
     print("Best Fit Descending: number of bins used: %d" % best_fit_desc(items))
